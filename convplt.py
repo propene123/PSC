a632ef472a353d1ea517e148607c3e8ff9433f26
Propene123 <benjamin.russell@durham.ac.uk>
2021-01-24 19:22:47 +0000

    hmmmmm

--- convplt.py
+++ convplt.py
@@ -4,29 +4,27 @@ from matplotlib import pyplot
 from scipy.optimize import curve_fit
 
 
-def objective(h,p,c):
-    return c*(h**p)
+def objective(x, m, c):
+    return m*x+c;
 
 
 
-dataframe = read_csv('./step1conv', header=None)
+dataframe = read_csv('./step2conv', header=None)
 data = dataframe.values
 x, y = data[:,0], data[:,-1]
 x_ar = np.array(list([x]))
 y_ar = np.array(list([y]))
 
 y_ar = 0.01 - y_ar
-foo, _ = curve_fit(objective, list(x_ar.flatten()), list(y_ar.flatten()))
-m,c = foo
-print(m)
 x_ar = 10.0/x_ar
 y_ar = np.log(y_ar)
 x_ar = np.log(x_ar)
+foo, _ = curve_fit(objective, list(x_ar.flatten()), list(y_ar.flatten()))
+m,c = foo
+print(m)
+print(c)
 
-
-
-
-pyplot.scatter(x_ar, y_ar)
+pyplot.plot(x_ar.flatten(), y_ar.flatten())
 pyplot.show()
 
 
